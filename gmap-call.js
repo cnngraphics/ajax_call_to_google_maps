62df1ac360b71493b88a4c0c7a16b9a414cf35c7
cnngraphics <cnngraphics@gmail.com>
2017-03-03 10:04:21 -0500

    Added comments to the script

--- gmap-call.js
+++ gmap-call.js
@@ -1,14 +1,16 @@
+//assumes that JQUERY is loaded
+
 $.ajax({
 url:"https://maps.googleapis.com/maps/api/geocode/json?",
-data: {address: 33071},
-type: "GET",
-dataType: "JSON"
+data: {address: 33071}, //will be converted to query string with &key=value
+type: "GET", //defines the type of reques
+dataType: "JSON" //expected return format for the data`
 })
-   .done(function(json){
+   .done(function(json){   //on success - call back function 
    console.log(json.results[0].address_components[3]);
    
 
-     }).fail(function(xhr, status,errorThrown){
+     }).fail(function(xhr, status,errorThrown){ //catching errors
 
       console.log("Sorry, issues - ");
       console.log("Error: "+errorThrown);
